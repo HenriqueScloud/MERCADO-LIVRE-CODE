03bbbf543156e0dc48c57e3e4b9932b4c5b61f6b
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-03-20 18:07:14 -0300

    new commit - components / albuns

---
+++ src/components/Albuns/Albuns.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/jsx-key */
+import { fetchAlbuns } from "../../dataBase/api";
+import { Cards } from "./Cards/Cards";
+import "./index.scss"
+
+function Albuns() {
+
+
+  return (
+    <section className="albuns_section">
+      <h1 className="albuns-title">Albuns Encontrados </h1>
+
+      < ul className="albun__cards">
+        {fetchAlbuns.length > 0 ? (
+          fetchAlbuns.map((card) => {
+            return <Cards key={card.id} Card={card} />;
+          })
+        ) : (
+          <p>No albums found</p>
+        )}
+      
+        
+      
+
+
+      </ul>
+    </section>
+  );
+}
+
+export default Albuns;
