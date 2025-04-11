2f102af4a2d18d5fddb2248f52d7941ac46363f0
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-10 21:34:09 -0300

    New commit - E-commerce (Desenvolvimento)

---
+++ src/pages/Store/Store.jsx
@@ -0,0 +1,12 @@
+import Albuns from "../../components/Albuns/Albuns"
+import FiltroGenero from "../../components/FilterGenero/FilterGenero"
+
+export const Store = () =>{
+    return (
+        <>
+        <FiltroGenero/>
+        <Albuns/>
+
+        </>
+    )
+}
