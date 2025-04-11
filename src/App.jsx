8f18a8f60ef941758ddb098ae5b81964b9160627
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-10 21:33:17 -0300

    New commit - E-commerce (Desenvolvimento)

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
 import "./styles/index.scss";
 
-import { HomePage } from "./routers/HomePage";
+// import { HomePage } from "./routers/HomePage";
+import Header from "./components/Header/Header";
+import { TodoContext } from "./providers/TodoContext";
+import { useContext } from "react";
+import { CartModal } from "./components/CartModal";
+import { HomePages } from "./routers/HomePage";
+import { Menu } from "./components/Menu/Menu";
 
 function App() {
-  return (
-    <>
-      <HomePage />
-    </>
-  );
+const { Theme,OnModal,OnMenu } = useContext(TodoContext);
+const html = document.querySelector("html");
+
+Theme == false
+  ? html.classList.add("darkMode")
+  : html.classList.remove("darkMode");
+
+return (
+  <main className="darkMode">
+    {OnModal && <CartModal />}
+    { OnMenu && <Menu/>}
+    <Header />
+    <HomePages />
+    <footer>
+      <h1>footer a ser desenvolvido !!</h1>
+    </footer>
+  </main>
+);
 }
 
 export default App;
+
+
+
+
