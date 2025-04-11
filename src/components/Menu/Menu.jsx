e60e138493c6b243f37f26377e8a59ac61f27c37
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-11 18:59:08 -0300

    New commit - E-commerce (Desenvolvimento)

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,24 +1,29 @@
 import { useContext } from "react";
 import { TodoContext } from "../../providers/TodoContext";
 import "../Menu/index.scss";
+import { Link } from "react-router-dom";
 export const Menu = () => {
-  const { OnMenu, setOnMenu, OnModal, setOnModal } = useContext(TodoContext);
+  const { OnMenu, setOnMenu, OnModal, setOnModal,setTheme, Theme } = useContext(TodoContext);
   return (
     <nav className="menu">
-      <div onClick={() => setOnMenu(!OnMenu)}/>
+      <div onClick={() => setOnMenu(!OnMenu)} />
       <ul className="menu-ul">
-        <li>
-          <a href="/">Loja</a>
-        </li>
+        
         <li>
           <a href="/about">Sobre</a>
         </li>
         <li>
-          <a href="/contats">Contatos</a>
+          <a href="/">Contatos</a>
         </li>
-        <li onClick={()=>setOnModal(!OnModal)}>
-          Carrinho
-        </li>
+        <li onClick={() => setOnModal(!OnModal)}>Carrinho</li>
+        <button
+          className="header__theme-btn"
+          onClick={() => {
+            setTheme(!Theme);
+          }}
+        ></button>
+
+        <Link to={"/about"} >sobre</Link>
       </ul>
     </nav>
   );
