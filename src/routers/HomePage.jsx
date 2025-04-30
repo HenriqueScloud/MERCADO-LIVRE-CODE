58ab32b167407c05a9daf2edebdc04b0fab59054
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-30 18:25:42 -0300

    New commit - E-commerce (Desenvolvimento)

--- src/routers/HomePage.jsx
+++ src/routers/HomePage.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Store } from "../pages/Store/Store";
 import { About } from "../pages/About/About";
 import { Contats } from "../pages/Contats/Contats";
 
 export const HomePages = () => {
   return (
-    <BrowserRouter>
       <Routes>
         {/* <Route path="/inicio" element={<Store />} /> PROJETAR UM INICIO  */}
         <Route path="/" element={<Store/>} />
         <Route path="/about" element={<About />} />
         <Route path="/contats" element={<Contats />} />
       </Routes>
-    </BrowserRouter>
   );
 }
