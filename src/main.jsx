dde6de22103d2f55e4dcd94c9fbe1e2d39da7715
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-30 18:25:07 -0300

    New commit - E-commerce (Desenvolvimento)

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import { TodoProvider } from "./providers/TodoContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
+    <BrowserRouter>
       <TodoProvider>
         <App />
       </TodoProvider>
+    </BrowserRouter>
   </StrictMode>
 );
