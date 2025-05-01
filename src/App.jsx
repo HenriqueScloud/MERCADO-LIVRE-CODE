4e0b0dee1c835fea1e5b84d599ec9ebd6a911127
Henrique S. Mendes <henriquesilvamendes076@gmail.com>
2025-04-30 23:31:10 -0300

    New commit - E-commerce (Desenvolvimento)

--- src/App.jsx
+++ src/App.jsx
@@ -5,32 +5,27 @@ import Header from "./components/Header/Header";
 import { TodoContext } from "./providers/TodoContext";
 import { useContext } from "react";
 import { CartModal } from "./components/CartModal";
-import { HomePages } from "./routers/HomePage";
 import { Menu } from "./components/Menu/Menu";
+import { Footer } from "./components/Footer/Footer";
+import { RoutersMain } from "./routers/RoutersMain";
 
 function App() {
-const { Theme,OnModal,OnMenu } = useContext(TodoContext);
-const html = document.querySelector("html");
+  const { Theme, OnModal, OnMenu } = useContext(TodoContext);
+  const html = document.querySelector("html");
 
-Theme == false
-  ? html.classList.add("darkMode")
-  : html.classList.remove("darkMode");
+  Theme == false
+    ? html.classList.add("darkMode")
+    : html.classList.remove("darkMode");
 
-return (
-  <main className="darkMode">
-    {OnModal && <CartModal />}
-    { OnMenu && <Menu/>}
-    <Header />
-    <HomePages />
-    <footer>
-      <h1>footer a ser desenvolvido !!</h1>
-    </footer>
-  </main>
-);
+  return (
+    <main className="darkMode">
+      {OnModal && <CartModal />}
+      {OnMenu && <Menu />}
+      <Header />
+      <RoutersMain />
+      <Footer />
+    </main>
+  );
 }
 
 export default App;
-
-
-
-
